375a3caeb2045196d55e03c89a509957149c975b
Robert Balicki <robert.balicki@gmail.com>
2019-07-30 22:39:46 -0400

    update things to be Smithy related

--- template/src/lib.rs
+++ template/src/lib.rs
@@ -1,7 +1,6 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console;
 
-
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
 //
@@ -10,18 +9,26 @@ use web_sys::console;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-
-// This is like the `main` function, except for JavaScript.
+// This is the entry point of your app
 #[wasm_bindgen(start)]
-pub fn main_js() -> Result<(), JsValue> {
+pub fn start() {
     // This provides better error messages in debug mode.
     // It's disabled in release mode so it doesn't bloat up the file size.
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
+    let node = util::document().query_selector("#app").unwrap().unwrap();
+    let app = smithy::smd!(
+      <h1>Hello, Smithy!</h1>
+      <p>
+        Thank you for trying Smithy!
+      </p>
+      <p>
+        Take a look at <code>src/lib.rs</code> and start editing!
+      </p>
+    );
+    smithy::mount(Box::new(app), node);
 
     // Your code goes here!
     console::log_1(&JsValue::from_str("Hello world!"));
-
-    Ok(())
 }
